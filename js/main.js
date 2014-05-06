1e98e44488f213fbb4f9780674ead99b7c58550b
Sergey <pettson@list.ru>
2014-05-06 10:13:15 +0400

    Начата реализация правых блоков

--- js/main.js
+++ js/main.js
@@ -3,13 +3,18 @@ $(document).ready(function() {
     $('.b-services-menu__link').click(function(e) {
         $subMenu = $(this).parent().find('ul');
 
-        if ($subMenu.is(':visible')) {
-            $subMenu.slideUp(300);
-        } else {
-            $subMenu.slideDown(300);
+        if ($subMenu.length) {
+
+            if ($subMenu.is(':visible')) {
+                $subMenu.slideUp(300);
+            } else {
+                $subMenu.slideDown(300);
+            }
+
+            e.preventDefault();
+
         }
 
-        e.preventDefault();
     });
 
 });
