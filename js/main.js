08a9487e663615e6ecfd37bc185127b86b3b8c37
Sergey <pettson@list.ru>
2014-05-06 09:54:34 +0400

    Реализовано левое меню

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
 
-    
+    $('.b-services-menu__link').click(function(e) {
+        $subMenu = $(this).parent().find('ul');
+
+        if ($subMenu.is(':visible')) {
+            $subMenu.slideUp(300);
+        } else {
+            $subMenu.slideDown(300);
+        }
+
+        e.preventDefault();
+    });
 
 });
